441615b1af39d1fc23973a053d1bf16e0585379e
Tiancheng Song <carwestsam@gmail.com>
2016-10-03 19:52:42 +0800

    add another line part

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import * as $ from "jquery";
 import {vm_Page} from "./vm_page";
-import {vm_Base} from "./vm_base";
 import {vm_Editor} from "./vm_editor";
 import {vm_LinePart} from "./vm_linepart";
 
@@ -27,3 +26,10 @@ vmLinePart1.width = 350;
 vmLinePart1.height = 100;
 vmLinePart1.baseline = 500;
 vmLinePart1.left = 100;
+
+let vmLinePart2 = new vm_LinePart(vmPage, $('#linepart2'));
+
+vmLinePart2.width = 350;
+vmLinePart2.height = 100;
+vmLinePart2.baseline = 300;
+vmLinePart2.left = 100;
