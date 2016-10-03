859e355c15350ba4a2f637eec75381e2f74afe46
Tiancheng Song <carwestsam@gmail.com>
2016-10-03 19:46:43 +0800

    add one line part

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import * as $ from "jquery";
 import {vm_Page} from "./vm_page";
 import {vm_Base} from "./vm_base";
 import {vm_Editor} from "./vm_editor";
+import {vm_LinePart} from "./vm_linepart";
 
 
 let vmEditor:vm_Editor = new vm_Editor(null,$('#editor'));
@@ -20,3 +21,9 @@ vmPage.baseline = 0;
 vmPage.left = 20;
 
 
+let vmLinePart1 = new vm_LinePart(vmPage, $('#linepart1'));
+
+vmLinePart1.width = 350;
+vmLinePart1.height = 100;
+vmLinePart1.baseline = 500;
+vmLinePart1.left = 100;
---
+++ src/vm_linepart.ts
@@ -0,0 +1,10 @@
+import {vm_Base} from "./vm_base";
+
+export class vm_LinePart extends vm_Base{
+    name:string = 'linepart';
+
+    constructor(parent:vm_Base, elem:any = null){
+        super(parent, elem);
+        this.elem.addClass(this.name);
+    }
+}
