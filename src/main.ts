13e5c32997d7a0279962fd5d28092be5de173b7c
Tiancheng Song <carwestsam@gmail.com>
2016-10-04 15:15:08 +0800

    生成一个看起来像谱面结构的页面

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import * as $ from "jquery";
+import * as _ from 'lodash';
 import {vm_Page} from "./vm_page";
 import {vm_Editor} from "./vm_editor";
 import {vm_LinePart} from "./vm_linepart";
@@ -7,44 +8,24 @@ import {vm_Measure} from "./vm_measure";
 
 let vmEditor:vm_Editor = new vm_Editor(null,$('#editor'));
 vmEditor.insertPage();
-// let vmPage:vm_Page = new vm_Page(vmEditor, $('#page'));
-//
+
 vmEditor.height = window.innerHeight - 20;
 vmEditor.width = window.innerWidth - 20;
 
 vmEditor.baseline = 10;
 vmEditor.left = 10;
 
-(<vm_Page>vmEditor.children[0]).insertLinePart();
-(<vm_Page>vmEditor.children[0]).insertLinePart();
-(<vm_Page>vmEditor.children[0]).insertLinePart();
+let vmPage = <vm_Page>vmEditor.children[0];
+vmPage.insertLinePart();
+vmPage.insertLinePart();
+vmPage.insertLinePart();
 
-//
-//
-// let vmLinePart1 = new vm_LinePart(vmPage, $('#linepart1'));
-//
-// vmLinePart1.width = 350;
-// vmLinePart1.height = 100;
-// vmLinePart1.baseline = 500;
-// vmLinePart1.left = 100;
-//
-// let vmLinePart2 = new vm_LinePart(vmPage, $('#linepart2'));
-//
-// vmLinePart2.width = 350;
-// vmLinePart2.height = 100;
-// vmLinePart2.baseline = 300;
-// vmLinePart2.left = 100;
-//
-// let vmMeasure1 = new vm_Measure(vmLinePart1, $('#measure1'));
-// let vmMeasure2 = new vm_Measure(vmLinePart1, $('#measure2'));
-// let vmMeasure3 = new vm_Measure(vmLinePart1, $('#measure3'));
-//
-// vmMeasure1.width = vmMeasure2.width = vmMeasure3.width = 100;
-// vmMeasure1.height = vmMeasure2.height = vmMeasure3.height = 50;
-// vmMeasure1.baseline = vmMeasure2.baseline = vmMeasure3.baseline = 20;
-// vmMeasure1.left = 20;
-// vmMeasure2.left = 120;
-// vmMeasure3.left = 220;
+_.each(vmPage.children, (child)=>{
+    let vmLinePart = <vm_LinePart>child;
+    _.times(3, ()=>vmLinePart.insertMeasure());
 
-
-
+    _.each(vmLinePart.children, (child)=>{
+        let vmMeasure = <vm_Measure>child;
+        _.times(4, ()=>vmMeasure.insertNote());
+    })
+});
--- src/vm_linepart.ts
+++ src/vm_linepart.ts
@@ -1,4 +1,5 @@
 import {vm_Base} from "./vm_base";
+import {vm_Measure} from "./vm_measure";
 
 export class vm_LinePart extends vm_Base{
     name:string = 'linepart';
@@ -11,5 +12,22 @@ export class vm_LinePart extends vm_Base{
             moveable: true,
             resizeable: true
         })
+    }
+
+    insertMeasure(){
+        if (this.children.length > 0 && this.children[this.children.length-1].left + 140 * 2 > this.width){
+            return ;
+        }
+
+        let vmMeasure = new vm_Measure(this);
+        vmMeasure.width = 140;
+        vmMeasure.height = 60;
+        vmMeasure.baseline = 20;
+
+        if (this.children.length == 1){
+            vmMeasure.left = 10;
+        }else if (this.children[this.children.length-2].left + 140 * 2 < this.width){
+            vmMeasure.left = this.children[this.children.length-2].left + 140;
+        }
     }
 }
